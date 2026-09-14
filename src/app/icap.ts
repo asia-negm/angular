2e483a37ecbec646c9691d835e22449b151e9d29
asia-negm <asiadeen23@gmail.com>
2026-09-14 18:39:23 +0300

    event section

--- src/app/icap.ts
+++ src/app/icap.ts
@@ -1,3 +1,21 @@
-export interface ICap {
-  
+export interface IAuthor {
+    name:string;
+    avatar:string;
+    role:string
 }
+
+export interface ICap{
+id:number;
+slug:string;
+title:string;
+ excerpt: string;
+    content: string;
+    category: string;
+    author: IAuthor;
+    image: string;
+    date: string;
+    readTime: string;
+    featured: boolean;
+    tags: string[];
+}
+
