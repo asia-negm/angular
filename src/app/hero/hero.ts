93cd80576d50c7dbcdc0a192b9cc95fb22734d01
asia-negm <asiadeen23@gmail.com>
2026-09-18 18:55:36 +0300

    project navbar:

--- src/app/hero/hero.ts
+++ src/app/hero/hero.ts
@@ -17,8 +17,7 @@ export class Hero {
   description =input('انغمس في أسرار المحترفين ونصائح عملية لتطوير مهاراتك في التصوير.');
   showBtn =input(true);
   showCards =input(true);
-
-    cards:ICard[]=[
+      cards:ICard[]=[
       {id:'0' ,icon:'fa-solid fa-newspaper' , h2:'+50' ,p:'مقالة'},
       {id:'1' ,icon:'fa-solid fa-users' , h2:'+10ألف' ,p:'قارئ'},
       {id:'2' ,icon:' fa-solid fa-folder-open' , h2:'4' ,p:'تصنيفات'},
